83665132a832e13240f151112a9953610090e24f
obafemi <oludahunsiobafemi18@gmail.com>
2022-06-18 11:40:38 +0100

    created and styled the sidebar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Sidebar from "../Sidebar/Sidebar";
 import "./Layout.scss";
 
 const Layout = () => {
-  return <div className="layout">Layout</div>;
+  return (
+    <div className="layout">
+      <Sidebar />
+    </div>
+  );
 };
 
 export default Layout;
