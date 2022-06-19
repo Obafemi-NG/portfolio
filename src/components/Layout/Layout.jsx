d3ec27ff9c2153ed51433fccc645bb91b5a1a5fa
obafemi <oludahunsiobafemi18@gmail.com>
2022-06-19 19:28:06 +0100

    ccreated  the layout for all pages

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
 import "./Layout.scss";
 
@@ -6,6 +7,12 @@ const Layout = () => {
   return (
     <div className="layout">
       <Sidebar />
+      <div className="page">
+        <span className="tags top-tag"> &lt;body&gt; </span>
+        <Outlet />
+        <span className="tags bottom-tag"> &lt;/body&gt; </span> <br />
+        <span className="tags bottom-tag-html"> &lt;/html&gt; </span>
+      </div>
     </div>
   );
 };
