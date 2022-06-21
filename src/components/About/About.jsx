207e1dc2d20fd0680c0b1f53137e350fdee7530f
obafemi <oludahunsiobafemi18@gmail.com>
2022-06-21 10:35:18 +0100

    added the loaders aimation from the react-loaders package. which displays pending the time a page is being rendered.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,6 +10,7 @@ import {
   faNodeJs,
   faReact,
 } from "@fortawesome/free-brands-svg-icons";
+import Loader from "react-loaders";
 
 const About = () => {
   const [letterName, setLetterName] = useState("text-animate");
@@ -19,56 +20,59 @@ const About = () => {
     }, 3000);
   }, []);
   return (
-    <div className="container about-page">
-      <div className="text-area">
-        <h1>
-          <AnimatedLetter
-            letterName={letterName}
-            strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
-            idx={15}
-          />
-        </h1>
-        <p>
-          I'm a very ambitious Front-End Developer looking for a role in an
-          established IT company with the opportunity to work with the latest
-          technologies on challenging and diverse projects.
-        </p>
-        <p>
-          {" "}
-          I'm quietly confident, naturally curious and perpetually working on
-          improving my chops on one design one design at a time.{" "}
-        </p>
-        <p>
-          {" "}
-          If I need to define myself in one sentence that would be a young,
-          vibrant, ambitious, a sports fanatics, a Madrististas, Soccer game
-          lover and tech-obsessed. And one of the quotes I adopted over time is
-          that <b> Obsession is a bigger drive that Talent</b>{" "}
-        </p>
-      </div>
-      <div className="cubic-container">
-        <div className="cubic-spinner">
-          <div className="face-1">
-            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-          </div>
-          <div className="face-2">
-            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-          </div>
-          <div className="face-3">
-            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-          </div>
-          <div className="face-4">
-            <FontAwesomeIcon icon={faJsSquare} color="#EFD810" />
-          </div>
-          <div className="face-5">
-            <FontAwesomeIcon icon={faGithubSquare} color="#000000" />
-          </div>
-          <div className="face-6">
-            <FontAwesomeIcon icon={faNodeJs} color="#3C873A" />
+    <>
+      <div className="container about-page">
+        <div className="text-area">
+          <h1>
+            <AnimatedLetter
+              letterName={letterName}
+              strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm a very ambitious Front-End Developer looking for a role in an
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          <p>
+            {" "}
+            I'm quietly confident, naturally curious and perpetually working on
+            improving my chops on one design one design at a time.{" "}
+          </p>
+          <p>
+            {" "}
+            If I need to define myself in one sentence that would be a young,
+            vibrant, ambitious, a sports fanatics, a Madrististas, Soccer game
+            lover and tech-obsessed. And one of the quotes I adopted over time
+            is that <b> Obsession is a bigger drive that Talent</b>{" "}
+          </p>
+        </div>
+        <div className="cubic-container">
+          <div className="cubic-spinner">
+            <div className="face-1">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face-2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face-3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face-4">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD810" />
+            </div>
+            <div className="face-5">
+              <FontAwesomeIcon icon={faGithubSquare} color="#000000" />
+            </div>
+            <div className="face-6">
+              <FontAwesomeIcon icon={faNodeJs} color="#3C873A" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import AnimatedLetter from "../AnimatedLetters/AnimatedLetters";
 
 import Logo from "./Logo/Logo";
+import Loader from "react-loaders";
 
 const Home = () => {
   const [letterName, setLetterName] = useState("text-animate");
@@ -38,34 +39,37 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container home-page">
-      <div className="text-area">
-        <h1>
-          <span className={`${letterName}`}>H</span>
-          <span className={`${letterName} _12`}>i,</span>
-          <br /> <span className={`${letterName} _13`}> I</span>
-          <span className={`${letterName} _14`}>'m </span>{" "}
-          <img src={logoSub} alt="developer" />
-          <AnimatedLetter
-            letterName={letterName}
-            strArray={nameArray}
-            idx={15}
-          />{" "}
-          <br />{" "}
-          <AnimatedLetter
-            letterName={letterName}
-            strArray={jobArray}
-            idx={21}
-          />{" "}
-        </h1>
-        <h2> Frontend Developer | Javascript Expert | Gamer </h2>
-        <button onClick={() => navigate("/contact")} className="flat-btn">
-          {" "}
-          CONTACT ME{" "}
-        </button>
+    <>
+      <div className="container home-page">
+        <div className="text-area">
+          <h1>
+            <span className={`${letterName}`}>H</span>
+            <span className={`${letterName} _12`}>i,</span>
+            <br /> <span className={`${letterName} _13`}> I</span>
+            <span className={`${letterName} _14`}>'m </span>{" "}
+            <img src={logoSub} alt="developer" />
+            <AnimatedLetter
+              letterName={letterName}
+              strArray={nameArray}
+              idx={15}
+            />{" "}
+            <br />{" "}
+            <AnimatedLetter
+              letterName={letterName}
+              strArray={jobArray}
+              idx={21}
+            />{" "}
+          </h1>
+          <h2> Frontend Developer | Javascript Expert | Gamer </h2>
+          <button onClick={() => navigate("/contact")} className="flat-btn">
+            {" "}
+            CONTACT ME{" "}
+          </button>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
